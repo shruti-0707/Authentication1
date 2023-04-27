ae7a4d02fe1824373e2b58d9f9e66a5ef33f2544
shruti-0707 <132011167+shruti-0707@users.noreply.github.com>
2023-04-28 01:28:00 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDR8A9v9LzowlHkjG4aG-wi2UECaurx24A",
+    authDomain: "authentication-42deb.firebaseapp.com",
+    projectId: "authentication-42deb",
+    storageBucket: "authentication-42deb.appspot.com",
+    messagingSenderId: "235934786824",
+    appId: "1:235934786824:web:251ee91acc8d1fd20478bd"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ welcome.js
@@ -0,0 +1,13 @@
+var db=firebase.firestore();
+firebase.auth().onAuthStateChanged((user)=>{
+    if(!user){
+        location.replace("index.html")
+    }else{
+        document.getElementById("user").innerHTML = "Hello, "+user.email
+    }
+})
+
+
+function logout(){
+    firebase.auth().signOut()
+}
